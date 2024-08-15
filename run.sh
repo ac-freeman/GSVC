f764435f0722341fb7fd2996f5f8b50ed3504697
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-15 15:44:14 +0800

    new

--- run.sh
+++ run.sh
@@ -16,4 +16,16 @@
 # Activate the environment if needed
 source activate torch     # Replace 'myenv' with the name of your conda environment
 
-sh ./scripts/gaussianimage_rs/kodak.sh
+data_path="/home/e/e1344641/data/kodak"
+
+if [ -z "$data_path" ]; then
+    echo "Error: No data_path provided."
+    echo "Usage: $0 <data_path>"
+    exit 1
+fi
+
+for num_points in 800 1000 3000 5000 7000 9000
+do
+srun python train.py -d $data_path \
+--data_name kodak --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000
+done
--- scripts/gaussianimage_cholesky/kodak.sh
+++ scripts/gaussianimage_cholesky/kodak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data_path="/home/e/e1344641/data/kodak"
+data_path=$1
 
 if [ -z "$data_path" ]; then
     echo "Error: No data_path provided."
@@ -10,6 +10,6 @@ fi
 
 for num_points in 800 1000 3000 5000 7000 9000
 do
-srun python train.py -d $data_path \
+CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
 --data_name kodak --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000
 done
