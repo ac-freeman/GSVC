3978ed3ad653c5c51bbb2780069bce1ad1c86c11
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-15 16:12:08 +0800

    m

--- rep.sh
+++ rep.sh
@@ -5,15 +5,11 @@
 #SBATCH --error=videogs_loss_error.txt  # Error log
 #SBATCH --time=12:00:00                 # Time limit hrs:min:sec
 #SBATCH --gres=gpu:a100-40:1
-#SBATCH --mem=16G                       # Memory required per node
 #SBATCH --mail-type=ALL                 # Get email for all status updates
 #SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
 
-# Load the necessary modules (if any, e.g., Python, CUDA, etc.)
-# module load python/3.8.5  # Load Python module, adjust the version accordingly
-# module load cuda/11.2     # Load CUDA module, adjust the version accordingly
+
 
 # Activate the environment if needed
 source activate torch     # Replace 'myenv' with the name of your conda environment
 
-srun ./scripts/gaussianimage_rs/kodak.sh
