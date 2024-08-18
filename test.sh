75f15a1d798fe1d42873b1ea470d799223c2e33d
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-18 16:35:14 +0800

    new

--- test.sh
+++ test.sh
@@ -12,4 +12,5 @@
 
 # Activate the environment if needed
 source activate torch     # Replace 'myenv' with the name of your conda environment
-srun python train_video.py
+srun python train_video.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --data_name HoneyBee
+srun python train_video.py --dataset /home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv --data_name Jockey
