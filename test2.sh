c9f8cbf61bf286ab1dc25a8e10f660ba114d077a
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-24 14:37:51 +0800

    new

---
+++ test2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=videogs_loss_job    # Job name
+#SBATCH --output=videogs_loss_output.txt # Standard output and error log
+#SBATCH --error=videogs_loss_error.txt  # Error log
+#SBATCH --time=72:00:00                 # Time limit hrs:min:sec
+#SBATCH --gres=gpu:a100-40:1
+#SBATCH --mail-type=ALL                 # Get email for all status updates
+#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
+
+# Activate the environment if needed
+source activate torch  # Replace 'torch' with the name of your conda environment
+
+# Define datasets and their corresponding names
+datasets=(
+  "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
+)
+
+# Loop through the datasets
+for dataset in "${datasets[@]}"; do
+  dataset_path=$(echo $dataset | cut -d' ' -f1)
+  data_name=$(echo $dataset | cut -d' ' -f2)
+  for num_points in 15000 20000 25000 30000 35000 40000 45000; do
+  # Run the training script for each dataset
+    srun python train_video.py --dataset $dataset_path --data_name $data_name --num_points $num_points
+    done
+done
+
