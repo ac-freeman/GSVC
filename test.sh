0ffdfc171cc5aa7ea28e62d83c83c07f9d7262e1
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-27 18:17:18 +0800

    new

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=videogs_loss_output.txt # Standard output and error log
 #SBATCH --error=videogs_loss_error.txt  # Error log
 #SBATCH --time=24:00:00                 # Time limit hrs:min:sec
-#SBATCH --gres=gpu:a100-40s:1
+#SBATCH --gres=gpu:a100-40:1
 #SBATCH --mail-type=ALL                 # Get email for all status updates
 #SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
 
