e407467b2bb3fa838c18eeae0ecdaf365b04e755
LonganWANG-cs <wanglongan1007@gmail.com>
2024-08-31 21:51:24 +0800

    new

--- train_video_pos.py
+++ train_video_pos.py
@@ -115,7 +115,7 @@ class SimpleTrainer2d:
         psnr = 10 * math.log10(1.0 / mse_loss.item())
         ms_ssim_value = ms_ssim(out["render"].float(), self.gt_image.float(), data_range=1, size_average=True).item()
         #self.logwriter.write("Test PSNR:{:.4f}, MS_SSIM:{:.6f}".format(psnr, ms_ssim_value))
-        if epoch==0 and self.save_imgs:
+        if (epoch==0 or (epoch+1)%100==0 )and self.save_imgs:
             # save_path_img = self.log_dir / "img"
             # save_path_img.mkdir(parents=True, exist_ok=True)
             # transform = transforms.ToPILImage()
