ac78e148c7e91f90e25a1422331efb0fd7f7f847
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-12 17:23:26 +0800

    new

--- test4.sh
+++ test4.sh
@@ -13,9 +13,7 @@ source activate torch  # Replace 'torch' with the name of your conda environment
 
 # Define datasets and their corresponding names
 datasets=(
-  "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
-  "/home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv HoneyBee"
-  "/home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv Jockey"
+  "/home/e/e1344641/data/UVG/Beauty/Beauty_transformed_1920x1080_120fps_420_8bit_YUV.yuv"
 )
 
 for dataset in "${datasets[@]}"; do
