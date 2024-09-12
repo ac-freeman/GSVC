10e645839f36778c6b7537dafe00191423d23b11
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-12 17:59:26 +0800

    new

--- test5.sh
+++ test5.sh
@@ -13,7 +13,9 @@ source activate torch  # Replace 'torch' with the name of your conda environment
 
 # Define datasets and their corresponding names
 datasets=(
-  "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
+  "/home/e/e1344641/data/UVG/Beauty/Beauty_transformed_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
+  "/home/e/e1344641/data/UVG/HoneyBee/HoneyBee_transformed_1920x1080_120fps_420_8bit_YUV.yuv HoneyBee"
+  "/home/e/e1344641/data/UVG/Jockey/Jockey_transformed_1920x1080_120fps_420_8bit_YUV.yuv Jockey"
 )
 
 for dataset in "${datasets[@]}"; do
@@ -22,7 +24,7 @@ for dataset in "${datasets[@]}"; do
   for num_points in 50000; do
   # Run the training script for each dataset
     for iterations in 30000; do
-      srun python train_video_density_first_frame.py --dataset $dataset_path --data_name $data_name --num_points $num_points --iterations $iterations
+      srun python train_video_density_pos.py --dataset $dataset_path --data_name $data_name --num_points $num_points --iterations $iterations
       done
     done
 done
