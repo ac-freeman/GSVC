e4b2355fad693272537df6c6b48e821d1428fcf9
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-14 19:01:07 +0800

    new

--- gaussianimage_cholesky_grad.py
+++ gaussianimage_cholesky_grad.py
@@ -387,7 +387,7 @@ class GaussianImage_Cholesky(nn.Module):
         if grad_xyz is None:
             raise RuntimeError("grad_xyz is None. Ensure self.get_xyz is a leaf tensor with requires_grad=True.")
         if (iter) % (self.densification_interval+1) == 0 and iter > 0 and isdensity:
-            self.density_control()
+            self.density_control(iter)
             # for param_group in self.optimizer.param_groups:
             #     for param in param_group['params']:
             #         print(param.size(), param.requires_grad)
