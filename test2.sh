399f75b9ede74f34a1d46053ac07ec23c9f20a37
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-20 15:30:47 +0800

    new

--- test2.sh
+++ test2.sh
@@ -12,10 +12,15 @@
 source activate torch  # Replace 'torch' with the name of your conda environment
 
 # Define datasets and their corresponding names
+# datasets=(
+#   "/home/e/e1344641/data/UVG/A1/A1_1920x1080_120fps_420_8bit_YUV.yuv A1"
+#   "/home/e/e1344641/data/UVG/A2/A2_1920x1080_120fps_420_8bit_YUV.yuv A2"
+#   "/home/e/e1344641/data/UVG/A3/A3_1920x1080_120fps_420_8bit_YUV.yuv A3"
+# )
 datasets=(
-  "/home/e/e1344641/data/UVG/A1/A1_1920x1080_120fps_420_8bit_YUV.yuv A1"
-  "/home/e/e1344641/data/UVG/A2/A2_1920x1080_120fps_420_8bit_YUV.yuv A2"
-  "/home/e/e1344641/data/UVG/A3/A3_1920x1080_120fps_420_8bit_YUV.yuv A3"
+  "/home/e/e1344641/data/UVG/B1/B1_1920x1080_120fps_420_8bit_YUV.yuv B1"
+  "/home/e/e1344641/data/UVG/B2/B2_1920x1080_120fps_420_8bit_YUV.yuv B2"
+  "/home/e/e1344641/data/UVG/B3/B3_1920x1080_120fps_420_8bit_YUV.yuv B3"
 )
 
 # Define additional parameters
