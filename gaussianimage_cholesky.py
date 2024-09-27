00723264f51d80a74ca5d983c265e115d8637449
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-27 14:38:28 +0800

    new

--- gaussianimage_cholesky.py
+++ gaussianimage_cholesky.py
@@ -439,11 +439,11 @@ class GaussianImage_Cholesky(nn.Module):
 
         # 对梯度幅值进行升序排序（最小的梯度在前）
         _, sorted_indices = torch.sort(grad_magnitude)
-        removal_rate_per_step = self.removal_rate/iter_threshold_remove
+        removal_rate_per_step = self.removal_rate/int(iter_threshold_remove/(self.densification_interval+1))
         if iter <= iter_threshold_remove:
             # 训练早期：只执行删除操作，减少总的高斯点数量
             remove_count = int(removal_rate_per_step * self.max_num_points)
-            print(self.removal_rate,iter_threshold_remove,removal_rate_per_step,int(self.removal_rate*self.max_num_points),remove_count)  
+            print(removal_rate_per_step,remove_count)  
             remove_indices = sorted_indices[:remove_count]
 
             # 删除选定的点
