52268ce02ce9ce705b1c7edbc5067a15f21a87be
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-27 14:33:09 +0800

    new

--- gaussianimage_cholesky.py
+++ gaussianimage_cholesky.py
@@ -443,7 +443,7 @@ class GaussianImage_Cholesky(nn.Module):
         if iter <= iter_threshold_remove:
             # 训练早期：只执行删除操作，减少总的高斯点数量
             remove_count = int(removal_rate_per_step * self.max_num_points)
-            print(remove_count)  
+            print(self.removal_rate,iter_threshold_remove,removal_rate_per_step,int(self.removal_rate*self.max_num_points),remove_count)  
             remove_indices = sorted_indices[:remove_count]
 
             # 删除选定的点
