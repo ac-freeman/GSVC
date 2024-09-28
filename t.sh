36ae00207267cd9cd129c4dd3f54c38bbe5f14b0
LonganWANG-cs <wanglongan1007@gmail.com>
2024-09-28 23:06:40 +0800

    new

--- t.sh
+++ t.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
 
-#SBATCH --job-name=videogs_loss_job    # Job name
-#SBATCH --output=videogs_loss_output.txt # Standard output and error log
-#SBATCH --error=videogs_loss_error.txt  # Error log
-#SBATCH --time=8:00:00                 # Time limit hrs:min:sec
-#SBATCH --gres=gpu:a100-40:1
-#SBATCH --mail-type=ALL                 # Get email for all status updates
-#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
-#SBATCH --mem=24G                       # Request 16GB of memory
-# Activate the environment if needed
-source activate torch  # Replace 'torch' with the name of your conda environment
 
 # Define datasets and their corresponding names
 datasets=(
