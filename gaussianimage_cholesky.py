b860caae01d6afccc84b474ee1889e0528ab98c3
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-06 15:32:08 +0800

    new

--- gaussianimage_cholesky.py
+++ gaussianimage_cholesky.py
@@ -499,6 +499,7 @@ class GaussianImage_Cholesky(nn.Module):
         elif iter == iter_threshold_remove:
             # 训练早期：只执行删除操作，减少总的高斯点数量
             remove_count = self._xyz.shape[0]-int(self.max_num_points * (1-self.removal_rate))
+            print(remove_count,self._xyz.shape[0])
             if remove_count>0:
                 remove_indices = sorted_indices[:remove_count]
 
@@ -509,7 +510,8 @@ class GaussianImage_Cholesky(nn.Module):
                 self._xyz = torch.nn.Parameter(self._xyz[keep_indices])
                 self._cholesky = torch.nn.Parameter(self._cholesky[keep_indices])
                 self._features_dc = torch.nn.Parameter(self._features_dc[keep_indices])
-                self._opacity = torch.nn.Parameter(self._opacity[keep_indices])   
+                self._opacity = torch.nn.Parameter(self._opacity[keep_indices])  
+                print(self._xyz.shape[0]) 
         # 更新优化器中的参数
         self.update_optimizer()
 
