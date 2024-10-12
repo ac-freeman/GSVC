cddbede5fa1e4ea37aa774c0f34c3263c5053a17
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-12 19:01:52 +0800

    new

--- gaussianimage_cholesky_Opacity.py
+++ gaussianimage_cholesky_Opacity.py
@@ -137,7 +137,7 @@ class GaussianImage_Cholesky(nn.Module):
         self.update_optimizer()
 
     def density_control_Opacity(self, iter,strat_iter_adaptive_control):
-        iter_threshold_remove =6000  # 根据训练计划调整这个阈值
+        iter_threshold_remove =1000  # 根据训练计划调整这个阈值
         opacity = self._opacity
         grad_magnitude =torch.norm(opacity, dim=1)
         _, sorted_indices = torch.sort(grad_magnitude)
@@ -155,8 +155,8 @@ class GaussianImage_Cholesky(nn.Module):
             self._cholesky = torch.nn.Parameter(self._cholesky[keep_indices])
             self._features_dc = torch.nn.Parameter(self._features_dc[keep_indices])
             self._opacity = torch.nn.Parameter(self._opacity[keep_indices])
-            if iter%3000==0:
-                self._opacity = torch.nn.Parameter(0.01 * torch.ones_like(self._opacity))
+            # if iter%3000==0:
+            #     self._opacity = torch.nn.Parameter(0.01 * torch.ones_like(self._opacity))
         elif iter == strat_iter_adaptive_control+iter_threshold_remove:
             # 训练早期：只执行删除操作，减少总的高斯点数量
             remove_count = self._xyz.shape[0]-int(self.max_num_points * (1-self.removal_rate))
