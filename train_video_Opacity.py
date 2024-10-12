20bb2cfccd2110c059087c3cabe504a2bf171961
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-12 19:33:41 +0800

    new

--- train_video_Opacity.py
+++ train_video_Opacity.py
@@ -102,12 +102,12 @@ class SimpleTrainer2d:
                     density_control=density_control-1
                     if density_control==0:
                         print(f"End ad at iteration {iter}")
-                    if density_control<0 and early_stopping(loss.item()) and early_stopping_PSNR(psnr.item()):
+                    if density_control<0 and early_stopping(loss.item()) and early_stopping_PSNR(psnr):
                         print(f"After adaptive control: Early stopping at iteration {iter}")
                         break
                 else:
                     strat_iter_adaptive_control=strat_iter_adaptive_control+1
-            elif early_stopping(loss.item()) and early_stopping_PSNR(psnr.item()):
+            elif early_stopping(loss.item()) and early_stopping_PSNR(psnr):
                 print(f"Early stopping at iteration {iter}")
                 break
         end_time = time.time() - start_time
