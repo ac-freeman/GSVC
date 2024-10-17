8b483b5398c7edb45e16e61c85f51535513dee87
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-17 16:34:04 +0800

    new

--- test_W_3.sh
+++ test_W_3.sh
@@ -27,7 +27,6 @@ loss_type="L2"
 for dataset in "${datasets[@]}"; do
   dataset_path=$(echo $dataset | cut -d' ' -f1)
   data_name=$(echo $dataset | cut -d' ' -f2)
-  # for num_points in 4000 6000 8000 10000 20000 30000 40000 50000 60000 70000; do
   for num_points in 27000 36000 45000 ; do
     for iterations in 100000; do
       pos_flag=""
