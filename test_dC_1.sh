6324ef5ea657998d99387aec7fc7878580a8a019
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-19 10:42:34 +0800

    new

--- test_dC_1.sh
+++ test_dC_1.sh
@@ -11,8 +11,8 @@
 # Activate the environment if needed
 source activate torch  # Replace 'torch' with the name of your conda environment
 
-srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 27000 --iterations 100000 --savdir result_density_Com --savdir_m models_density_Com --is_ad
-srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 36000 --iterations 100000 --savdir result_density_Com --savdir_m models_density_Com --is_ad
-srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 45000 --iterations 100000 --savdir result_density_Com --savdir_m models_density_Com --is_ad
+srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 30000 --iterations 100000 --savdir result_density_Com2 --savdir_m models_density_Com2 --is_ad
+srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 40000 --iterations 100000 --savdir result_density_Com2 --savdir_m models_density_Com2 --is_ad
+srun python train_video_Combine.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 50000 --iterations 100000 --savdir result_density_Com2 --savdir_m models_density_Com2 --is_ad
 
 
