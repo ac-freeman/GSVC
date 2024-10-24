3d201b1c8b11d4a636f7f9802471b08214c35b31
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-24 15:23:23 +0800

    new

--- train_video_rgbW.py
+++ train_video_rgbW.py
@@ -121,8 +121,8 @@ class SimpleTrainer2d:
                 print(f"Early stopping at iteration {iter}")
                 break
             if iter % 1000 == 0:
-                self.print_stats("get_features", self.gaussian_model.get_features)
-                self.print_stats("features_dc", self.gaussian_model.get_features)
+                self.print_stats("get_features", self.gaussian_model.get_features[:, 0])
+                self.print_stats("features_dc", self.gaussian_model._features_dc[:, 0])
                 self.print_stats("rgb_W", self.gaussian_model.rgb_W)
 
         end_time = time.time() - start_time
