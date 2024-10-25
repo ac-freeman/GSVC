af34b33e9d3aabcd0383cb638d9c728767d1c27f
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-25 19:04:45 +0800

    new

--- test_W_3.sh
+++ test_W_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=test_dW_2    # Job name
+#SBATCH --job-name=test_W_2    # Job name
 #SBATCH --output=videogs_loss_output.txt # Standard output and error log
 #SBATCH --error=videogs_loss_error.txt  # Error log
 #SBATCH --time=24:00:00                 # Time limit hrs:min:sec
