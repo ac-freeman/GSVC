149e6e7e376e96eb63823103d419936d57748971
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-26 11:00:44 +0800

    new

--- gaussianimage_cholesky_rgbW.py
+++ gaussianimage_cholesky_rgbW.py
@@ -152,8 +152,8 @@ class GaussianImage_Cholesky(nn.Module):
             return
         # rgb_weight = torch.norm(self.rgb_W, dim=1)
         # grad_magnitude =torch.norm(rgb_weight, dim=1)
-        # rgb_weight = self.rgb_W.squeeze(1)
-        _, sorted_indices = torch.sort(self.get_rgb_W)
+        rgb_weight = self.get_rgb_W.squeeze(1)
+        _, sorted_indices = torch.sort(rgb_weight)
         removal_rate_per_step = self.removal_rate/int(iter_threshold_remove/(self.densification_interval))
         if iter < strat_iter_adaptive_control+iter_threshold_remove:
             remove_count = int(removal_rate_per_step * self.max_num_points)
