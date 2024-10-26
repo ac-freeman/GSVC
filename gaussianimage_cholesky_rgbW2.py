1cb9f82f558b49339c9af9de403b8ecccf68698a
LonganWANG-cs <wanglongan1007@gmail.com>
2024-10-26 20:05:06 +0800

    new

--- gaussianimage_cholesky_rgbW2.py
+++ gaussianimage_cholesky_rgbW2.py
@@ -177,7 +177,7 @@ class GaussianImage_Cholesky(nn.Module):
         elif iter == strat_iter_adaptive_control+iter_threshold_remove:
             # 训练早期：只执行删除操作，减少总的高斯点数量
             remove_count = self._xyz.shape[0]-int(self.max_num_points * (1-self.removal_rate))
-            self._features_dc = self.get_features
+            self._features_dc = self._features_dc*self.rgb_W
             self.rgb_W = self.rgb_W.detach()
             self.rgb_W.data.fill_(1)
             #print(remove_count,self._xyz.shape[0])
