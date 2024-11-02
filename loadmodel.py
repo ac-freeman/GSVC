0b79ffb3e098d0b0f3755e0239c6ea70e6964514
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-02 14:18:55 +0800

    new

---
+++ loadmodel.py
@@ -0,0 +1,164 @@
+import math
+import time
+from pathlib import Path
+import argparse
+import numpy as np
+import torch
+import sys
+from PIL import Image
+import torch.nn.functional as F
+from pytorch_msssim import ms_ssim
+from utils import *
+from tqdm import tqdm
+import random
+import torchvision.transforms as transforms
+import cv2
+from pathlib import Path
+import os
+
+
+class LoadGaussians:
+    """Trains random 2d gaussians to fit an image."""
+    def __init__(
+        self,
+        image,
+        model_path = None,
+        args = None,
+    ):
+        self.device = torch.device("cuda:0")
+        self.gt_image = image_to_tensor(image).to(self.device)
+        self.num_points=20000
+        self.data_name=args.data_name
+        BLOCK_H, BLOCK_W = 16, 16
+        self.H, self.W = self.gt_image.shape[2], self.gt_image.shape[3]
+        self.save_imgs = args.save_imgs
+        from Gaussian2D import GaussianImage_Cholesky
+        self.gaussian_model = GaussianImage_Cholesky(num_points=self.num_points, H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W, 
+        device=self.device).to(self.device)
+        if model_path is not None:
+            print(f"loading model path:{model_path}")
+            checkpoint = torch.load(model_path, map_location=self.device)
+            model_dict = self.gaussian_model.state_dict()
+            pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+            model_dict.update(pretrained_dict)
+            self.gaussian_model.load_state_dict(model_dict)
+
+    def render(self):     
+        psnr_value, ms_ssim_value,img = self.test()
+        with torch.no_grad():
+            self.gaussian_model.eval()
+            test_start_time = time.time()
+            for i in range(100):
+                _ = self.gaussian_model()
+            test_end_time = (time.time() - test_start_time)/100       
+        return psnr_value, ms_ssim_value, 1/test_end_time, img
+    
+    def test(self):
+        self.gaussian_model.eval()
+        with torch.no_grad():
+            out = self.gaussian_model()
+            out_image = out["render"]
+        mse_loss = F.mse_loss(out_image.float(), self.gt_image.float())
+        psnr = 10 * math.log10(1.0 / mse_loss.item())
+        ms_ssim_value = ms_ssim(out_image.float(), self.gt_image.float(), data_range=1, size_average=True).item()
+        transform = transforms.ToPILImage()
+        img = transform(out_image.float().squeeze(0))
+        return psnr, ms_ssim_value,img
+
+def image_to_tensor(img: Image.Image):
+    transform = transforms.ToTensor()
+    img_tensor = transform(img).unsqueeze(0)  # [1, C, H, W]
+    return img_tensor
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description="Example training script.")
+    parser.add_argument(
+        "-d", "--dataset", type=str, default='/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv', help="Dataset Path"
+    )
+    parser.add_argument(
+        "-d", "--model_path", type=str, default='/home/e/e1344641/GaussianVideo/models/Models/Beauty/GaussianImage_Cholesky_100000_30000/gmodels_state_dict.pth', help="Model Path"
+    )
+    parser.add_argument(
+        "--data_name", type=str, default='Beauty', help="Training dataset"
+    )
+    parser.add_argument(
+        "--fps", type=int, default=120, help="number of frames per second (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--num_points",
+        type=int,
+        default=4000,
+        help="2D GS points (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--width", type=int, default=1920, help="width (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--height", type=int, default=1080, help="height (default: %(default)s)"
+    )
+    parser.add_argument("--model_path", type=str, default=None, help="Path to a checkpoint")
+    parser.add_argument("--savdir", type=str, default="result", help="Path to results")
+    parser.add_argument("--seed", type=float, default=1, help="Set random seed for reproducibility")
+    args = parser.parse_args(argv)
+    return args
+
+def main(argv):
+    args = parse_args(argv)
+    savdir=args.savdir
+    fps=args.fps
+    width = args.width
+    height = args.height
+    model_path=args.model_path
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+        random.seed(args.seed)
+        torch.cuda.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        np.random.seed(args.seed)
+    logwriter = LogWriter(Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}"))
+    psnrs, ms_ssims, eval_fpses = [], [], []
+    image_h, image_w = 0, 0
+    video_frames = process_yuv_video(args.dataset, width, height)
+    image_length,start=len(video_frames),0
+    # image_length=5
+    img_list=[]
+    for i in range(start, start+image_length):
+        frame_num=i+1
+        Gaussianframe = LoadGaussians(image=video_frames[i], model_path=None, args=args)
+        psnr, ms_ssim,eval_fps, img = Gaussianframe.render()
+        img_list.append(img)
+        psnrs.append(psnr)
+        ms_ssims.append(ms_ssim)
+        eval_fpses.append(eval_fps)
+        torch.cuda.empty_cache()
+        if i==0 or (i+1)%1==0:
+            logwriter.write("Frame_{}: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, FPS:{:.4f}".format(frame_num, Gaussianframe.H, Gaussianframe.W, psnr, ms_ssim, eval_fps))
+    file_size = os.path.getsize(model_path)
+    avg_psnr = torch.tensor(psnrs).mean().item()
+    avg_ms_ssim = torch.tensor(ms_ssims).mean().item()
+    avg_eval_fps = torch.tensor(eval_fpses).mean().item()
+    logwriter.write("Average: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}, Size:{:.4f}, Gaussian_number:{:.4f}".format(
+        height, width, avg_psnr, avg_ms_ssim, avg_eval_fps, file_size/ (1024 * 1024)))
+    
+
+    video_path = Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}/video")
+    video_path.mkdir(parents=True, exist_ok=True)
+    filename = "video.mp4"
+    output_size = (width, height)
+    video = cv2.VideoWriter(str(video_path / filename), cv2.VideoWriter_fourcc(*'mp4v'), fps, output_size)
+    for img in tqdm(img_list, desc="Processing images", unit="image"):    
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        video.write(img_cv)
+    video.release()
+    print("video.mp4: MP4 video created successfully.")
+
+
+
+if __name__ == "__main__":
+    
+    main(sys.argv[1:])
+
+
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=test    # Job name
 #SBATCH --output=videogs_loss_output.txt # Standard output and error log
 #SBATCH --error=videogs_loss_error.txt  # Error log
-#SBATCH --time=24:00:00                 # Time limit hrs:min:sec
+#SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --gres=gpu:a100-40:1
 #SBATCH --mail-type=ALL                 # Get email for all status updates
 #SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
@@ -11,42 +11,9 @@
 # Activate the environment if needed
 source activate torch  # Replace 'torch' with the name of your conda environment
 
-# Define datasets and their corresponding names
-datasets=(
-  "/home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv HoneyBee"
-)
-
-# Define additional parameters
-savdir="result_test"
-savdir_m="model_test"
-is_pos=False
-is_ad=False
-loss_type="L2"
-for dataset in "${datasets[@]}"; do
-  dataset_path=$(echo $dataset | cut -d' ' -f1)
-  data_name=$(echo $dataset | cut -d' ' -f2)
-  # for num_points in 30000 40000 50000; do
-  for num_points in 4000; do
-    for iterations in 10000; do
-      pos_flag=""
-      ad_flag=""
-
-      # 检查布尔值并构建相应的命令行参数
-      if [ "$is_pos" = True ]; then
-        pos_flag="--is_pos"
-      fi
-
-
-      if [ "$is_ad" = True ]; then
-        ad_flag="--is_ad"
-      fi
-
-      # Run the training script for each dataset with additional parameters
-      srun python train_video_r.py --loss_type $loss_type --dataset $dataset_path \
-        --data_name $data_name --num_points $num_points --iterations $iterations \
-        --savdir $savdir --savdir_m $savdir_m \
-        $pos_flag $ad_flag 
-    done
-  done
-done
-
+dataset_path="/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv"
+model_path="/home/e/e1344641/GaussianVideo/models/Models/Beauty/GaussianImage_Cholesky_100000_30000/gmodels_state_dict.pth"
+data_name="Beauty"
+num_points=30000
+savdir="test"
+srun python train_video_r.py --dataset $dataset_path --data_name $data_name --num_points $num_points --savdir $savdir --model_path $model_path
