3067b281c1ffd991de1abf8943771efaffabfa25
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-02 18:55:34 +0800

    new

--- loadmodel.py
+++ loadmodel.py
@@ -43,27 +43,38 @@ class LoadGaussians:
             model_dict.update(pretrained_dict)
             self.gaussian_model.load_state_dict(model_dict)
 
-    def render(self):     
-        psnr_value, ms_ssim_value,img = self.test()
-        with torch.no_grad():
-            self.gaussian_model.eval()
-            test_start_time = time.time()
-            for i in range(100):
-                _ = self.gaussian_model()
-            test_end_time = (time.time() - test_start_time)/100       
-        return psnr_value, ms_ssim_value, 1/test_end_time, img
+    # def render(self):     
+    #     psnr_value, ms_ssim_value,img = self.test()
+    #     with torch.no_grad():
+    #         self.gaussian_model.eval()
+    #         test_start_time = time.time()
+    #         for i in range(100):
+    #             _ = self.gaussian_model()
+    #         test_end_time = (time.time() - test_start_time)/100       
+    #     return psnr_value, ms_ssim_value, 1/test_end_time, img
     
-    def test(self):
+    # def test(self):
+    #     self.gaussian_model.eval()
+    #     with torch.no_grad():
+    #         out = self.gaussian_model()
+    #         out_image = out["render"]
+    #     mse_loss = F.mse_loss(out_image.float(), self.gt_image.float())
+    #     psnr = 10 * math.log10(1.0 / mse_loss.item())
+    #     ms_ssim_value = ms_ssim(out_image.float(), self.gt_image.float(), data_range=1, size_average=True).item()
+    #     transform = transforms.ToPILImage()
+    #     img = transform(out_image.float().squeeze(0))
+    #     return psnr, ms_ssim_value,img
+
+
+    def render(self):     
         self.gaussian_model.eval()
         with torch.no_grad():
             out = self.gaussian_model()
             out_image = out["render"]
-        mse_loss = F.mse_loss(out_image.float(), self.gt_image.float())
-        psnr = 10 * math.log10(1.0 / mse_loss.item())
-        ms_ssim_value = ms_ssim(out_image.float(), self.gt_image.float(), data_range=1, size_average=True).item()
         transform = transforms.ToPILImage()
         img = transform(out_image.float().squeeze(0))
-        return psnr, ms_ssim_value,img
+        return img
+    
 
 def image_to_tensor(img: Image.Image):
     transform = transforms.ToTensor()
@@ -101,56 +112,94 @@ def parse_args(argv):
     args = parser.parse_args(argv)
     return args
 
+# def main(argv):
+#     step=10
+#     args = parse_args(argv)
+#     savdir=args.savdir
+#     fps=args.fps/step
+#     width = args.width
+#     height = args.height
+#     model_path=args.model_path
+#     num_points=args.num_points
+#     device=torch.device("cuda:0")
+#     if args.seed is not None:
+#         torch.manual_seed(args.seed)
+#         random.seed(args.seed)
+#         torch.cuda.manual_seed(args.seed)
+#         torch.backends.cudnn.deterministic = True
+#         torch.backends.cudnn.benchmark = False
+#         np.random.seed(args.seed)
+#     logwriter = LogWriter(Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}"))
+#     psnrs, ms_ssims, eval_fpses = [], [], []
+#     image_h, image_w = 0, 0
+#     video_frames = process_yuv_video(args.dataset, width, height)
+#     image_length,start=len(video_frames),0
+#     # image_length=5
+#     img_list=[]
+#     print(f"loading model path:{model_path}")
+#     gmodels_state_dict = torch.load(model_path,map_location=device)
+#     for i in tqdm(range(start, start + image_length), desc="Processing Frames"):
+#         if i==0 or i%step==0:
+#             frame_num=i+1
+#             modelid=f"frame_{i + 1}"
+#             Model = gmodels_state_dict[modelid]
+#             Gaussianframe = LoadGaussians(num_points=num_points,image=video_frames[i], Model=Model,device=device,args=args)
+#             psnr, ms_ssim,eval_fps, img = Gaussianframe.render()
+#             img_list.append(img)
+#             psnrs.append(psnr)
+#             ms_ssims.append(ms_ssim)
+#             eval_fpses.append(eval_fps)
+#             torch.cuda.empty_cache()
+#             # logwriter.write(
+#             #         "Frame_{}: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, FPS:{:.4f}\n".format(
+#             #             frame_num, Gaussianframe.H, Gaussianframe.W, psnr, ms_ssim, eval_fps
+#             #         )
+#             #     )
+#     file_size = os.path.getsize(model_path)
+#     avg_psnr = torch.tensor(psnrs).mean().item()
+#     avg_ms_ssim = torch.tensor(ms_ssims).mean().item()
+#     avg_eval_fps = torch.tensor(eval_fpses).mean().item()
+#     logwriter.write("Average: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f},FPS:{:.4f}, Size:{:.4f}".format(
+#         height, width, avg_psnr, avg_ms_ssim, avg_eval_fps, file_size/ (1024 * 1024)))
+    
+
+#     video_path = Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}/video")
+#     video_path.mkdir(parents=True, exist_ok=True)
+#     filename = "video.mp4"
+#     output_size = (width, height)
+#     video = cv2.VideoWriter(str(video_path / filename), cv2.VideoWriter_fourcc(*'mp4v'), fps, output_size)
+#     for img in tqdm(img_list, desc="Processing images", unit="image"):    
+#         if img.mode != 'RGB':
+#             img = img.convert('RGB')
+#         img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+#         video.write(img_cv)
+#     video.release()
+#     print("video.mp4: MP4 video created successfully.")
+
 def main(argv):
     step=10
     args = parse_args(argv)
     savdir=args.savdir
-    fps=args.fps/step
+    fps=5
     width = args.width
     height = args.height
     model_path=args.model_path
     num_points=args.num_points
     device=torch.device("cuda:0")
-    if args.seed is not None:
-        torch.manual_seed(args.seed)
-        random.seed(args.seed)
-        torch.cuda.manual_seed(args.seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-        np.random.seed(args.seed)
-    logwriter = LogWriter(Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}"))
-    psnrs, ms_ssims, eval_fpses = [], [], []
-    image_h, image_w = 0, 0
     video_frames = process_yuv_video(args.dataset, width, height)
     image_length,start=len(video_frames),0
-    # image_length=5
+    image_length=50
     img_list=[]
     print(f"loading model path:{model_path}")
     gmodels_state_dict = torch.load(model_path,map_location=device)
     for i in tqdm(range(start, start + image_length), desc="Processing Frames"):
         if i==0 or i%step==0:
-            frame_num=i+1
             modelid=f"frame_{i + 1}"
             Model = gmodels_state_dict[modelid]
             Gaussianframe = LoadGaussians(num_points=num_points,image=video_frames[i], Model=Model,device=device,args=args)
-            psnr, ms_ssim,eval_fps, img = Gaussianframe.render()
+            img = Gaussianframe.render()
             img_list.append(img)
-            psnrs.append(psnr)
-            ms_ssims.append(ms_ssim)
-            eval_fpses.append(eval_fps)
             torch.cuda.empty_cache()
-            # logwriter.write(
-            #         "Frame_{}: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, FPS:{:.4f}\n".format(
-            #             frame_num, Gaussianframe.H, Gaussianframe.W, psnr, ms_ssim, eval_fps
-            #         )
-            #     )
-    file_size = os.path.getsize(model_path)
-    avg_psnr = torch.tensor(psnrs).mean().item()
-    avg_ms_ssim = torch.tensor(ms_ssims).mean().item()
-    avg_eval_fps = torch.tensor(eval_fpses).mean().item()
-    logwriter.write("Average: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f},FPS:{:.4f}, Size:{:.4f}".format(
-        height, width, avg_psnr, avg_ms_ssim, avg_eval_fps, file_size/ (1024 * 1024)))
-    
 
     video_path = Path(f"./Loadmodel/{savdir}/{args.data_name}/{args.num_points}/video")
     video_path.mkdir(parents=True, exist_ok=True)
@@ -164,8 +213,6 @@ def main(argv):
         video.write(img_cv)
     video.release()
     print("video.mp4: MP4 video created successfully.")
-
-
 
 if __name__ == "__main__":
     
