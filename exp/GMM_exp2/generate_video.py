65ae8e45753348c389061b95d1d921051b0ac02a
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-10 11:02:45 +0800

    new

--- exp/GMM_exp2/generate_video.py
+++ exp/GMM_exp2/generate_video.py
@@ -35,7 +35,7 @@ with open(output_video_path, 'wb') as yuv_file:
                 position = (width - 300, height // 2)
 
             # 创建一个带有对称渐变纹理的小球图案
-            radius = 50
+            radius = 150
             ball_texture = np.zeros((radius * 2, radius * 2, 3), dtype=np.uint8)
             
             # 使用径向对称渐变纹理
