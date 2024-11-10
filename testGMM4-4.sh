ff0b7f8127ca31f770cc447f3d45d0df0749775f
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-10 12:28:38 +0800

    new

--- testGMM4-4.sh
+++ testGMM4-4.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-#SBATCH --job-name=GMM    # Job name
+#SBATCH --job-name=testGMM4-4    # Job name
 #SBATCH --output=videogs_loss_output.txt # Standard output and error log
 #SBATCH --error=videogs_loss_error.txt  # Error log
-#SBATCH --time=24:00:00                 # Time limit hrs:min:sec
+#SBATCH --time=48:00:00                 # Time limit hrs:min:sec
 #SBATCH --gres=gpu:h100-47:1
 #SBATCH --mail-type=ALL                 # Get email for all status updates
 #SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
-#SBATCH --mem=16G                    # Request 16GB of memory
+#SBATCH --mem=16G                       # Request 16GB of memory
 # Activate the environment if needed
 source activate torch  # Replace 'torch' with the name of your conda environment
+
 # Define datasets and their corresponding names
 datasets=(
-  "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
-  "/home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv HoneyBee"
   "/home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv Jockey"
 )
+
 # Define additional parameters
-savdir="GMM3"
-savdir_m="GMM3_m"
+savdir="result_GMM"
+savdir_m="models_GMM"
 is_pos=False
 is_ad=True
 loss_type="L2"
 for dataset in "${datasets[@]}"; do
   dataset_path=$(echo $dataset | cut -d' ' -f1)
   data_name=$(echo $dataset | cut -d' ' -f2)
-  for num_points in 5000; do
+  # for num_points in 30000 40000 50000; do
+  # for num_points in 30000 40000 50000 60000 70000 80000; do
+  for num_points in 70000 80000; do
     for iterations in 100000; do
       pos_flag=""
       ad_flag=""
@@ -41,11 +43,10 @@ for dataset in "${datasets[@]}"; do
       fi
 
       # Run the training script for each dataset with additional parameters
-      srun python train_video_test2.py --loss_type $loss_type --dataset $dataset_path \
+      srun python train_video_GMM2.py --loss_type $loss_type --dataset $dataset_path \
         --data_name $data_name --num_points $num_points --iterations $iterations \
         --savdir $savdir --savdir_m $savdir_m \
         $pos_flag $ad_flag 
     done
   done
 done
-
