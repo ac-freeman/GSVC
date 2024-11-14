9b9d98cc4bab38633a05a02f35f5eef43723ed81
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-14 12:50:24 +0800

    new

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=test    # Job name
+#SBATCH --output=videogs_loss_output.txt # Standard output and error log
+#SBATCH --error=videogs_loss_error.txt  # Error log
+#SBATCH --time=24:00:00                 # Time limit hrs:min:sec
+#SBATCH --gres=gpu:a100-40:1
+#SBATCH --mail-type=ALL                 # Get email for all status updates
+#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
+#SBATCH --mem=16G                    # Request 16GB of memory
+# Activate the environment if needed
+source activate torch  # Replace 'torch' with the name of your conda environment
+
+python train_video_densification.py --loss_type L2 --dataset /home/e/e1344641/GaussianVideo/exp/GMM_exp/test_video.yuv --data_name test1 --num_points 10000 --iterations 25000 --savdir test1 --savdir_m test1 --is_rm --save_everyimgs --is_pos
+python train_video_densification.py --loss_type L2 --dataset /home/e/e1344641/GaussianVideo/exp/GMM_exp/test_video.yuv --data_name test2 --num_points 10000 --iterations 25000 --savdir test1 --savdir_m test1 --is_ad --is_rm --save_everyimgs --is_pos
+python train_video_densification.py --loss_type L2 --dataset /home/e/e1344641/GaussianVideo/exp/GMM_exp2/output_video1.yuv --data_name test3 --num_points 10000 --iterations 25000 --savdir test1 --savdir_m test1 --is_ad --is_rm --save_everyimgs --is_pos
+python train_video_DG.py --loss_type L2 --dataset /home/e/e1344641/GaussianVideo/exp/GMM_exp2/output_video1.yuv --data_name test4 --num_points 10000 --iterations 25000 --savdir test1 --savdir_m test1 --is_ad --is_rm --save_everyimgs --is_pos
+
