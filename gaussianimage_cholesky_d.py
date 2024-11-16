599ea7658c56f92ebbde9ec18797cbcb8c1aaf16
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-16 13:09:57 +0800

    new

--- gaussianimage_cholesky_d.py
+++ gaussianimage_cholesky_d.py
@@ -128,7 +128,7 @@ class GaussianImage_Cholesky(nn.Module):
 
     def adaptive_control(self, iter):
         iter_threshold_remove =500  # 根据训练计划调整这个阈值
-        iter_threshold_add = 1
+        iter_threshold_add = 500
         densification_num = int(self.max_num_points * self.removal_rate)
         if iter>iter_threshold_add+iter_threshold_remove or iter<iter_threshold_add:
             if iter == 1 and densification_num > 0:
