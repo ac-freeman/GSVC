ad152d6838a299adaabca8e52c8a2382ef916ff3
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-23 14:35:09 +0800

    new

---
+++ test_GSP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=test1    # Job name
+#SBATCH --output=videogs_loss_output.txt # Standard output and error log
+#SBATCH --error=videogs_loss_error.txt  # Error log
+#SBATCH --time=24:00:00                 # Time limit hrs:min:sec
+#SBATCH --gres=gpu:a100-40:1
+#SBATCH --mail-type=ALL                 # Get email for all status updates
+#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
+#SBATCH --mem=16G                    # Request 16GB of memory
+# Activate the environment if needed
+source activate torch  # Replace 'torch' with the name of your conda environment
+
+python train_video_DKS_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 50000 --iterations 100000 --savdir test5 --savdir_m GSPtest --is_rm --save_everyimgs --is_pos
+python train_video_DKS_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --data_name HoneyBee --num_points 50000 --iterations 100000 --savdir test5 --savdir_m GSPtest --is_rm --save_everyimgs --is_pos
+python train_video_DKS_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv --data_name Jockey --num_points 50000 --iterations 100000 --savdir test5 --savdir_m GSPtest --is_rm --save_everyimgs --is_pos
+
--- train_video_GSP_test.py
+++ train_video_GSP_test.py
@@ -13,7 +13,6 @@ from utils import *
 from tqdm import tqdm
 import random
 import torchvision.transforms as transforms
-from sklearn.mixture import GaussianMixture
 class SimpleTrainer2d:
     """Trains random 2d gaussians to fit an image."""
     def __init__(
@@ -45,13 +44,14 @@ class SimpleTrainer2d:
         self.iterations = iterations
         self.densification_interval=args.densification_interval
         self.save_imgs = args.save_imgs
-        self.save_everyimgs = args.save_everyimgs
+        # self.save_everyimgs = args.save_everyimgs
+        self.save_everyimgs=True
         self.log_dir = Path(f"./checkpoints/{savdir}/{args.data_name}/{args.model_name}_{args.iterations}_{args.num_points}")
         self.isdensity=isdensity
         self.isremoval=isremoval
         self.loss_type = loss_type
         if model_name == "GaussianImage_Cholesky":
-            from gaussianimage_cholesky_d import GaussianImage_Cholesky
+            from gaussianimage_cholesky_d_test import GaussianImage_Cholesky
             self.gaussian_model = GaussianImage_Cholesky(loss_type=self.loss_type, opt_type="adan", num_points=self.num_points,max_num_points=self.max_num_points,densification_interval=self.densification_interval,iterations=self.iterations, H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W, 
             device=self.device, lr=args.lr, quantize=False,removal_rate=removal_rate,isdensity=self.isdensity,isremoval=self.isremoval).to(self.device)
         if model_path is not None:
@@ -80,7 +80,7 @@ class SimpleTrainer2d:
         progress_bar = tqdm(range(1, int(self.iterations)+1), desc="Training progress")
         self.gaussian_model.train()
         start_time = time.time()
-        early_stopping = EarlyStopping(patience=100, min_delta=1e-7)
+        early_stopping = EarlyStopping(patience=100, min_delta=1e-6)
         early_stopping_PSNR = EarlyStopping(patience=100, min_delta=1e-4)
         stabel_control=5000
         for iter in range(1, int(self.iterations)+1):
@@ -117,25 +117,6 @@ class SimpleTrainer2d:
         filtered_Gmodel['_features_dc']=self.gaussian_model.get_features
         return psnr_value, ms_ssim_value, end_time, test_end_time, 1/test_end_time, filtered_Gmodel, img, num_gaussian_points, loss
     
-
-    def pre_train(self):     
-        progress_bar = tqdm(range(1, int(self.iterations)+1), desc="Training progress")
-        self.gaussian_model.train()
-        for iter in range(1, int(self.iterations)+1):
-            loss, psnr = self.gaussian_model.pre_train_iter(self.gt_image)
-            with torch.no_grad():
-                if iter % 10 == 0:
-                    progress_bar.set_postfix({f"Loss":f"{loss.item():.{7}f}", "PSNR":f"{psnr:.{4}f},"})
-                    progress_bar.update(10)
-        progress_bar.close()
-        Gmodel =self.gaussian_model.state_dict()
-        filtered_Gmodel = {
-            k: v for k, v in Gmodel.items()
-            if k in ['_xyz', '_cholesky']
-        }
-        filtered_Gmodel['_features_dc']=self.gaussian_model.get_features
-        return filtered_Gmodel, loss
-
     def test(self,frame,num_gaussian_points,ispos):
         self.gaussian_model.eval()
         with torch.no_grad():
@@ -294,61 +275,54 @@ def main(argv):
     image_h, image_w = 0, 0
     video_frames = process_yuv_video(args.dataset, width, height)
     image_length,start=len(video_frames),0
-    # image_length=5
+    image_length=1
+    start=0
     Gmodel=None
     img_list=[]
     gmodels_state_dict = {}
     num_gaussian_points_dict={}
-
-
-
-
-    output_path_K_frames = Path(f"./checkpoints/{savdir}/{args.data_name}/K_frames.txt")
-    if output_path_K_frames.exists():
-        # If exists, read the file and assign values to K_frames
-        with open(output_path_K_frames, "r") as f:
-            K_frames = [int(line.strip()) for line in f.readlines()]
+    for i in range(start, start+image_length):
+        frame_num=i+1
+        if frame_num ==1 or frame_num%50==0:
+            trainer = SimpleTrainer2d(image=video_frames[i],frame_num=frame_num,savdir=savdir,loss_type=loss_type, num_points=args.num_points,max_num_points=args.num_points,
+                    iterations=args.iterations, model_name=args.model_name, args=args, model_path=None,Trained_Model=None,isdensity=False,isremoval=is_rm,removal_rate=removal_rate)
+        else:
+            trainer = SimpleTrainer2d(image=video_frames[i],frame_num=frame_num,savdir=savdir,loss_type=loss_type, num_points=num_gaussian_points,max_num_points=args.num_points,
+                iterations=args.iterations, model_name=args.model_name, args=args, model_path=None,Trained_Model=Gmodel,isdensity=is_ad,isremoval=False,removal_rate=removal_rate)
+        psnr, ms_ssim, training_time, eval_time, eval_fps, Gmodel, img, num_gaussian_points, loss = trainer.train(i,ispos)
+        img_list.append(img)
+        psnrs.append(psnr)
+        ms_ssims.append(ms_ssim)
+        training_times.append(training_time) 
+        eval_times.append(eval_time)
+        eval_fpses.append(eval_fps)
+        gaussian_number.append(num_gaussian_points)
+        image_h += trainer.H
+        image_w += trainer.W
+        gmodels_state_dict[f"frame_{frame_num}"] = Gmodel
+        num_gaussian_points_dict[f"frame_{frame_num}"]=num_gaussian_points
+        torch.cuda.empty_cache()
+        if i==0 or (i+1)%1==0:
+            logwriter.write("Frame_{}: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}, Loss:{:.4f}".format(frame_num, trainer.H, trainer.W, psnr, ms_ssim, training_time, eval_time, eval_fps, loss))
+    torch.save(gmodels_state_dict, gmodel_save_path / "gmodels_state_dict.pth")
+    file_size = os.path.getsize(os.path.join(gmodel_save_path, 'gmodels_state_dict.pth'))
+    with open(Path(f"./checkpoints/{savdir}/{args.data_name}/{args.model_name}_{args.iterations}_{args.num_points}") / "num_gaussian_points.txt", 'w') as f:
+        for key, value in num_gaussian_points_dict.items():
+            f.write(f'{key}: {value}\n')
+    avg_psnr = torch.tensor(psnrs).mean().item()
+    avg_ms_ssim = torch.tensor(ms_ssims).mean().item()
+    avg_training_time = torch.tensor(training_times).mean().item()
+    avg_eval_time = torch.tensor(eval_times).mean().item()
+    avg_eval_fps = torch.tensor(eval_fpses).mean().item()
+    avg_h = image_h//image_length
+    avg_w = image_w//image_length
+    gaussians = sum(gaussian_number) / len(gaussian_number)
+    logwriter.write("Average: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}, Size:{:.4f}, Gaussian_number:{:.4f}".format(
+        avg_h, avg_w, avg_psnr, avg_ms_ssim, avg_training_time, avg_eval_time, avg_eval_fps, file_size/ (1024 * 1024),gaussians))
+    if ispos:
+        generate_video_density(savdir,img_list, args.data_name, args.model_name,args.fps,args.iterations,args.num_points,origin=False)    
     else:
-        loss_list=[]
-        for i in range(start, start+image_length):
-            frame_num=i+1
-            if frame_num ==1:
-                loss_extractor_K = SimpleTrainer2d(image=video_frames[i],frame_num=frame_num,savdir=savdir,loss_type=loss_type, num_points=5000, 
-                    iterations=500, model_name=args.model_name, args=args, model_path=None,Trained_Model=None,isdensity=False,isremoval=False,removal_rate=removal_rate)
-                Gmodel,_= loss_extractor_K.pre_train()
-                loss=0
-                loss_list.append(0)
-            else:
-                loss_extractor_K = SimpleTrainer2d(image=video_frames[i],frame_num=frame_num,savdir=savdir,loss_type=loss_type, num_points=5000, 
-                    iterations=500, model_name=args.model_name, args=args, model_path=None,Trained_Model=None,isdensity=False,isremoval=False,removal_rate=removal_rate)
-                loss_extractor_P = SimpleTrainer2d(image=video_frames[i],frame_num=frame_num,savdir=savdir,loss_type=loss_type, num_points=5000, 
-                    iterations=100, model_name=args.model_name, args=args, model_path=None,Trained_Model=Gmodel,isdensity=False,isremoval=False,removal_rate=removal_rate)  
-                Gmodel, loss_K = loss_extractor_K.pre_train()
-                _, loss_P = loss_extractor_P.pre_train()
-                loss_list.append(loss_P-loss_K)
-        loss_list = np.array([
-            v.detach().cpu().numpy() if isinstance(v, torch.Tensor) else v for v in loss_list
-        ])
-        values_to_normalize = loss_list[1:]
-        min_value = np.min(values_to_normalize)
-        max_value = np.max(values_to_normalize)
-        # Normalized values in range [0, 1]
-        normalized_loss_list = [loss_list[0]] + [(v - min_value) / (max_value - min_value) for v in values_to_normalize]
-        output_path = Path(f"./checkpoints/{savdir}/{args.data_name}/{args.model_name}_{args.iterations}_{args.num_points}/loss_list.txt")
-        with open(output_path, "w") as f:
-            for index, loss in enumerate(normalized_loss_list, start=1):
-                f.write(f"Frame {index}: {loss}\n")
-
-        outlier_indices = detect_outliers_mean_diff(normalized_loss_list)
-        K_frames = [int(x + 1) for x in outlier_indices]
-        K_frames = np.insert(K_frames, 0, 1)
-        K_frames = K_frames.astype(int)
-        output_path_K_frames = Path(f"./checkpoints/{savdir}/{args.data_name}/K_frames.txt")
-        with open(output_path_K_frames, "w") as f:
-            for frame in K_frames:
-                f.write(f"{frame}\n")
-    print("K-frames:", K_frames)
-
+        generate_video_density(savdir,img_list, args.data_name, args.model_name,args.fps,args.iterations,args.num_points,origin=True)  
     
 if __name__ == "__main__":
     
