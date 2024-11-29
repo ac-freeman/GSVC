4a27e8ad1eaddabfddffd35b2fc368421e3c0275
LonganWANG-cs <wanglongan1007@gmail.com>
2024-11-29 12:35:59 +0800

    new

---
+++ 2DGaussianSplats.py
@@ -0,0 +1,65 @@
+from gsplat.project_gaussians_2d import project_gaussians_2d
+from gsplat.rasterize_sum import rasterize_gaussians_sum
+from utils import *
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class GaussianImage_Cholesky(nn.Module):
+    def __init__(self, loss_type="L2", **kwargs):
+        super().__init__()
+        self.init_num_points = kwargs["num_points"]
+        self.H, self.W = kwargs["H"], kwargs["W"]
+        self.BLOCK_W, self.BLOCK_H = kwargs["BLOCK_W"], kwargs["BLOCK_H"]
+        self.tile_bounds = (
+            (self.W + self.BLOCK_W - 1) // self.BLOCK_W,
+            (self.H + self.BLOCK_H - 1) // self.BLOCK_H,
+            1,
+        ) # 
+        self.device = kwargs["device"]
+        self._xyz = nn.Parameter(torch.atanh(2 * (torch.rand(self.init_num_points, 2) - 0.5)))
+        self._cholesky = nn.Parameter(torch.rand(self.init_num_points, 3))
+        self._features_dc = nn.Parameter(torch.rand(self.init_num_points, 3))
+        self.last_size = (self.H, self.W)
+        self.register_buffer('background', torch.ones(3))
+        self.register_buffer('bound', torch.tensor([0.5, 0.5]).view(1, 2))
+        self.register_buffer('cholesky_bound', torch.tensor([0.5, 0, 0.5]).view(1, 3))
+
+    @property
+    def get_xyz(self):
+        return torch.tanh(self._xyz)
+    
+    @property
+    def get_features(self):
+        return self._features_dc
+    
+    @property
+    def get_cholesky_elements(self):
+        return self._cholesky+self.cholesky_bound
+
+    def forward_pos(self,num_points):
+        features_dc = torch.ones(num_points, 3).to(self.device)
+        cholesky = torch.full((num_points, 3), 1.0).to(self.device)
+        _opacity = torch.ones(num_points, 1).to(self.device)
+        self.xys, depths, self.radii, conics, num_tiles_hit = project_gaussians_2d(self.get_xyz, cholesky+self.cholesky_bound, self.H, self.W, self.tile_bounds)
+        out_img = rasterize_gaussians_sum(self.xys, depths, self.radii, conics, num_tiles_hit,
+                features_dc, _opacity, self.H, self.W, self.BLOCK_H, self.BLOCK_W, background=self.background, return_alpha=False)
+        out_img = torch.clamp(out_img, 0, 1) #[H, W, 3]
+        out_img = out_img.view(-1, self.H, self.W, 3).permute(0, 3, 1, 2).contiguous()
+        return {"render_pos": out_img}
+    
+    def forward(self):
+        _opacity = torch.ones(self._xyz.shape[0], 1).to(self.device)
+        self.xys, depths, self.radii, conics, num_tiles_hit = project_gaussians_2d(self.get_xyz, self.get_cholesky_elements, self.H, self.W, self.tile_bounds)
+        out_img = rasterize_gaussians_sum(self.xys, depths, self.radii, conics, num_tiles_hit,
+                self.get_features, _opacity, self.H, self.W, self.BLOCK_H, self.BLOCK_W, background=self.background, return_alpha=False)
+        out_img = torch.clamp(out_img, 0, 1) #[H, W, 3]
+        out_img = out_img.view(-1, self.H, self.W, 3).permute(0, 3, 1, 2).contiguous()
+        return {"render": out_img}
+
+    
+    
+    
+
+    
--- Sup/com/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 自动化 Git 添加、提交和推送
-git add .
-git commit -m "new"
-git push origin master
-
