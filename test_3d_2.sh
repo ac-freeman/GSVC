d40142bfd38e802de178076d238effebe4a70ef8
LonganWANG-cs <wanglongan1007@gmail.com>
2024-12-05 14:41:16 +0800

    new

---
+++ test_3d_2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_3d_2    # Job name
+#SBATCH --output=videogs_loss_output.txt # Standard output and error log
+#SBATCH --error=videogs_loss_error.txt  # Error log
+#SBATCH --time=72:00:00                 # Time limit hrs:min:sec
+#SBATCH --gres=gpu:h100-47:1
+#SBATCH --mail-type=ALL                 # Get email for all status updates
+#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
+#SBATCH --mem=16G                    # Request 16GB of memory
+# Activate the environment if needed
+source activate torch  # Replace 'torch' with the name of your conda environment
+
+python train_video_3dGS.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv  --data_name HoneyBee --num_points 10000 --savdir 3dGS --savdir_m m_3dGS --iterations 50000
+python train_video_3dGS.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv  --data_name HoneyBee --num_points 20000 --savdir 3dGS --savdir_m m_3dGS --iterations 50000
+python train_video_3dGS.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv  --data_name HoneyBee --num_points 30000 --savdir 3dGS --savdir_m m_3dGS --iterations 50000
+python train_video_3dGS.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv  --data_name HoneyBee --num_points 40000 --savdir 3dGS --savdir_m m_3dGS --iterations 50000
+python train_video_3dGS.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv  --data_name HoneyBee --num_points 50000 --savdir 3dGS --savdir_m m_3dGS --iterations 50000
--- train_video_3dGS.py
+++ train_video_3dGS.py
@@ -81,14 +81,14 @@ class SimpleTrainer2d:
         self.gaussian_model.train()
         start_time = time.time()
         early_stopping = EarlyStopping(patience=100, min_delta=1e-7)
-        early_stopping_PSNR = EarlyStopping(patience=100, min_delta=1e-4)
-        stabel_control=5000
         for iter in range(1, int(self.iterations)+1):
             loss, psnr = self.gaussian_model.train_iter(self.gt_image)
             with torch.no_grad():
                 if iter % 10 == 0:
                     progress_bar.set_postfix({f"Loss":f"{loss.item():.{7}f}", "PSNR":f"{psnr:.{4}f},"})
                     progress_bar.update(10)
+                if early_stopping(loss.item()):
+                    break
         end_time = time.time() - start_time
         progress_bar.close()
         num_gaussian_points =self.gaussian_model._xyz.size(0)
