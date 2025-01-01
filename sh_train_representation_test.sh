722747e0eb8cbf7f0f53070f9be06bea012c31d9
LonganWANG-cs <wanglongan1007@gmail.com>
2025-01-01 16:39:32 +0800

    new

---
+++ sh_train_representation_test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#SBATCH --job-name=T_rep    # Job name
+#SBATCH --output=T_rep_output.txt # Standard output and error log
+#SBATCH --error=T_rep_error.txt  # Error log
+#SBATCH --time=48:00:00                 # Time limit hrs:min:sec
+#SBATCH --gres=gpu:h100-47:1
+#SBATCH --mail-type=ALL                 # Get email for all status updates
+#SBATCH --mail-user=wanglongan@comp.nus.edu.sg # Email for notifications
+#SBATCH --mem=16G                    # Request 16GB of memory
+# Activate the environment if needed
+source activate torch  # Replace 'torch' with the name of your conda environment
+
+# Define datasets and their corresponding names
+# datasets=(
+#   "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
+# )
+
+
+datasets=(
+  "/home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv Beauty"
+  "/home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv HoneyBee"
+  "/home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv Jockey"
+)
+
+
+# Define additional parameters
+savdir="result_none"
+savdir_m="models_none"
+is_pos=False
+is_ad=False
+is_rm=False
+loss_type="L2"
+for dataset in "${datasets[@]}"; do
+  dataset_path=$(echo $dataset | cut -d' ' -f1)
+  data_name=$(echo $dataset | cut -d' ' -f2)
+  for num_points in  9000  18000 27000 36000 45000; do
+    for iterations in 100000; do
+      pos_flag=""
+      ad_flag=""
+      rm_flag=""
+      if [ "$is_pos" = True ]; then
+        pos_flag="--is_pos"
+      fi
+
+
+      if [ "$is_ad" = True ]; then
+        ad_flag="--is_ad"
+      fi
+
+      if [ "$is_rm" = True ]; then
+        rm_flag="--is_rm"
+      fi
+
+      srun python train_video_Full_test.py --loss_type $loss_type --dataset $dataset_path \
+        --data_name $data_name --num_points $num_points --iterations $iterations \
+        --savdir $savdir --savdir_m $savdir_m \
+        $pos_flag $ad_flag $rm_flag
+    done
+  done
+done
+
