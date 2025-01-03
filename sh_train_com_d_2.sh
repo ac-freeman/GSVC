8af1a9ded556f19eb7cf14f275077559d270b11a
LonganWANG-cs <wanglongan1007@gmail.com>
2025-01-03 11:45:48 +0800

    new

--- sh_train_com_d_2.sh
+++ sh_train_com_d_2.sh
@@ -17,11 +17,57 @@ datasets=(
   "/home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv Jockey"
 )
 
-python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_10000/gmodels_state_dict.pth --data_name HoneyBee --num_points 10000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
-python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_20000/gmodels_state_dict.pth --data_name HoneyBee --num_points 20000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
-python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_30000/gmodels_state_dict.pth --data_name HoneyBee --num_points 30000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
-python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_40000/gmodels_state_dict.pth --data_name HoneyBee --num_points 40000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
-python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_50000/gmodels_state_dict.pth --data_name HoneyBee --num_points 50000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
+
+# Define additional parameters
+savdir="result_K10"
+savdir_m="models_K10"
+is_pos=False
+is_ad=True
+is_rm=True
+loss_type="L2"
+for dataset in "${datasets[@]}"; do
+  dataset_path=$(echo $dataset | cut -d' ' -f1)
+  data_name=$(echo $dataset | cut -d' ' -f2)
+  for num_points in  10000  20000 30000 40000 50000; do
+    for iterations in 100000; do
+      pos_flag=""
+      ad_flag=""
+      rm_flag=""
+      if [ "$is_pos" = True ]; then
+        pos_flag="--is_pos"
+      fi
+
+
+      if [ "$is_ad" = True ]; then
+        ad_flag="--is_ad"
+      fi
+
+      if [ "$is_rm" = True ]; then
+        rm_flag="--is_rm"
+      fi
+
+      srun python train_video_Full_test2.py --loss_type $loss_type --dataset $dataset_path \
+        --data_name $data_name --num_points $num_points --iterations $iterations \
+        --savdir $savdir --savdir_m $savdir_m \
+        $pos_flag $ad_flag $rm_flag
+    done
+  done
+done
+
+
+
+python train_Compress_delta2.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_10000/gmodels_state_dict.pth --data_name HoneyBee --K_frame_path /home/e/e1344641/GaussianVideo/checkpoints/result_K10/HoneyBee/K-frames.txt --num_points 10000 --savdir Compress_delta_2 --savdir_m Compress_delta_2_modles --iterations 50000 --is_rm
+python train_Compress_delta2.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_20000/gmodels_state_dict.pth --data_name HoneyBee --K_frame_path /home/e/e1344641/GaussianVideo/checkpoints/result_K10/HoneyBee/K-frames.txt --num_points 20000 --savdir Compress_delta_2 --savdir_m Compress_delta_2_modles --iterations 50000 --is_rm
+python train_Compress_delta2.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_30000/gmodels_state_dict.pth --data_name HoneyBee --K_frame_path /home/e/e1344641/GaussianVideo/checkpoints/result_K10/HoneyBee/K-frames.txt --num_points 30000 --savdir Compress_delta_2 --savdir_m Compress_delta_2_modles --iterations 50000 --is_rm
+python train_Compress_delta2.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_40000/gmodels_state_dict.pth --data_name HoneyBee --K_frame_path /home/e/e1344641/GaussianVideo/checkpoints/result_K10/HoneyBee/K-frames.txt --num_points 40000 --savdir Compress_delta_2 --savdir_m Compress_delta_2_modles --iterations 50000 --is_rm
+python train_Compress_delta2.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_50000/gmodels_state_dict.pth --data_name HoneyBee --K_frame_path /home/e/e1344641/GaussianVideo/checkpoints/result_K10/HoneyBee/K-frames.txt --num_points 50000 --savdir Compress_delta_2 --savdir_m Compress_delta_2_modles --iterations 50000 --is_rm
+
+
+# python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_10000/gmodels_state_dict.pth --data_name HoneyBee --num_points 10000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
+# python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_20000/gmodels_state_dict.pth --data_name HoneyBee --num_points 20000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
+# python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_30000/gmodels_state_dict.pth --data_name HoneyBee --num_points 30000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
+# python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_40000/gmodels_state_dict.pth --data_name HoneyBee --num_points 40000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
+# python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/models/models_dd/HoneyBee/GaussianImage_Cholesky_100000_50000/gmodels_state_dict.pth --data_name HoneyBee --num_points 50000 --savdir Compress_delta_1 --savdir_m Compress_delta_1_modles --iterations 50000 --is_rm
 
 
 # python train_Compress_delta.py --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/checkpoints/models_none/HoneyBee/GaussianVideo_100000_9000/gmodels_state_dict.pth --data_name HoneyBee --num_points 9000 --savdir Compress_delta_none --savdir_m Compress_delta_none_modles --iterations 50000 
