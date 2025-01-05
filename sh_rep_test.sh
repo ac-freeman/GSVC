5ae63bee3ffffbf6cd3686a317673f2d18ede812
LonganWANG-cs <wanglongan1007@gmail.com>
2025-01-05 14:17:15 +0800

    new

--- sh_rep_test.sh
+++ sh_rep_test.sh
@@ -11,4 +11,4 @@
 # Activate the environment if needed
 source activate torch  # Replace 'torch' with the name of your conda environment
 
-srun python test_rasterizer.py --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/checkpoints_quant/Compress_delta_4_modles/Beauty/GaussianVideo_50000_10000/gmodels_state_dict.pth --data_name Beauty --num_points 9000 --savdir d_video --is_rm 
+srun python test_rasterizer.py --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/checkpoints_quant/Compress_delta_4_modles/Beauty/GaussianVideo_50000_10000/gmodels_state_dict.pth --data_name Beauty --num_points 10000 --savdir d_video --is_rm 
