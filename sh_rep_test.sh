7805ccdfb8223eed35dacdc73fa01b65913f5236
LonganWANG-cs <wanglongan1007@gmail.com>
2025-01-05 14:12:15 +0800

    new

--- sh_rep_test.sh
+++ sh_rep_test.sh
@@ -11,11 +11,4 @@
 # Activate the environment if needed
 source activate torch  # Replace 'torch' with the name of your conda environment
 
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --data_name HoneyBee --num_points 50000 --iterations 100001 --savdir test_result --savdir_m test_models --is_ad --is_rm --is_pos --save_everyimgs
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/HoneyBee/HoneyBee_1920x1080_120fps_420_8bit_YUV.yuv --data_name HoneyBee --num_points 45000 --iterations 100001 --savdir testGI_result --savdir_m testGI_models --is_pos --save_everyimgs
-
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 50000 --iterations 100001 --savdir test_result --savdir_m test_models --is_ad --is_rm --is_pos --save_everyimgs
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --data_name Beauty --num_points 45000 --iterations 100001 --savdir testGI_result --savdir_m testGI_models --is_pos --save_everyimgs
-
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv --data_name Jockey --num_points 50000 --iterations 100001 --savdir test_result --savdir_m test_models --is_ad --is_rm --is_pos --save_everyimgs
-srun python train_test.py --loss_type L2 --dataset /home/e/e1344641/data/UVG/Jockey/Jockey_1920x1080_120fps_420_8bit_YUV.yuv --data_name Jockey --num_points 45000 --iterations 100001 --savdir testGI_result --savdir_m testGI_models --is_pos --save_everyimgs
+srun python test_rasterizer.py --dataset /home/e/e1344641/data/UVG/Beauty/Beauty_1920x1080_120fps_420_8bit_YUV.yuv --model_path /home/e/e1344641/GaussianVideo/checkpoints_quant/Compress_delta_4_modles/Beauty/GaussianVideo_50000_10000/gmodels_state_dict.pth --data_name Beauty --num_points 9000 --savdir d_video --is_rm 
