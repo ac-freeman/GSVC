30012a2960173fc837b4c45076e9f59a2c6742d1
LonganWANG-cs <wanglongan1007@gmail.com>
2025-01-14 22:25:57 +0800

    new

--- GaussianSplats_GaussianVideo_debug.py
+++ GaussianSplats_GaussianVideo_debug.py
@@ -94,7 +94,7 @@ class GaussianVideo_frame(nn.Module):
         out_img = (
                 torch.zeros(self.H, self.W, self.get_features.shape[-1], device=self._xyz.device)* self.background
             )
-        # out_img = torch.clamp(out_img, 0, 1) #[H, W, 3]
+        out_img = torch.clamp(out_img, 0, 1) #[H, W, 3]
         out_img = out_img.view(-1, self.H, self.W, 3).permute(0, 3, 1, 2).contiguous()
         return {"render": out_img}
 
